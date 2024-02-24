ba0a357fdf7fce7b32a5319770a56ec7e45a24f0
JEOVAHMARIO <135380760+JEOVAHMARIO@users.noreply.github.com>
2024-02-24 12:37:27 -0300

    frameworks

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
-// Middleware para registrar data, hora e rota de cada solicitação
 app.use((req, res, next) => {
   const currentDate = new Date().toLocaleDateString();
   const currentTime = new Date().toLocaleTimeString();
@@ -12,19 +11,15 @@ app.use((req, res, next) => {
   
   console.log(requestInfo);
 
-  // Chame next() para passar para o próximo middleware ou rota
   next();
 });
 
-// Configuração do EJS e Body Parser
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configuração para servir arquivos estáticos da pasta 'public'
 app.use(express.static('public'));
 
-// Rota para processar o formulário e renderizar area.ejs
 app.post('/area', (req, res) => {
     const nome = req.body.nome;
     const lado = parseFloat(req.body.lado);
@@ -37,25 +32,22 @@ app.post('/area', (req, res) => {
       area = 2 * Math.pow(lado, 2) * (1 + Math.sqrt(2));
       explanation = 'explicação personalizada aqui';
     } else {
-      isErro = true; // Definir isErro como verdadeiro se o lado for inválido
+      isErro = true;
     }
   
     res.render('area', { isErro, nome, lado, area, explanation });
 });
 
-// Rota para renderizar o resultado da área mesmo sem enviar o formulário
 app.get('/area', (req, res) => {
     // Você pode definir valores padrão ou deixar em branco
     res.render('area', { isErro: false, nome: '', lado: 0, area: 0, explanation: '' });
 });
 
 
-// Rota para renderizar o index.ejs
 app.get('/index', (req, res) => {
   res.render('index');
 });
 
-// Rota para renderizar o sobre.ejs
 app.get('/sobre', (req, res) => {
   res.render('sobre', {
     autor: {
@@ -69,13 +61,10 @@ app.get('/sobre', (req, res) => {
   });
 });
 
-// Middleware para lidar com rotas não encontradas
 app.use((req, res) => {
     res.status(404).render('error404', { url: req.originalUrl });
   });
-  
 
-// Inicia o servidor na porta especificada
 app.listen(PORT, () => {
   console.log(`Servidor iniciado na porta ${PORT}`);
 });
