8431459667a7ccd170daa08fa884fd11b0b5a59c
jeovah <jeovah9@gmail.com>
2024-03-16 08:58:03 -0300

    Add manifest

---
+++ app/public/_js/ServiceWorker.js
@@ -0,0 +1,27 @@
+// self.addEventListener('install', function (event) {
+//     event.waitUntil(
+//         caches.open('cache-v1').then(function (cache) {
+//             return cache.addAll([
+//                 '/',
+//                 '/index',
+//                 '/_img/logo.png'
+//             ]);
+//         })
+//     );
+// });
+self.addEventListener('fetch', function (event) {
+    console.log('fetchpwa')
+    let req = new Request(event.request, {
+        headers: { "foo": "bar" }
+    });
+    event.respondWith(fetch(req));
+});
+
+
+// self.addEventListener('fetch', function (event) {
+//     // event.respondWith(
+//     //     caches.match(event.request).then(function (response) {
+//     //         return response || fetch(event.request);
+//     //     })
+//     // );
+// });
